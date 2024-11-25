b62bcde75f57779204f23d602308896ee1a9a4db
Sam281055 <Sl280155@gmail.com>
2024-11-25 15:39:52 -0300

    Se agrega landingPage

--- app/header.tsx
+++ app/header.tsx
@@ -1,18 +1,20 @@
 import { Button } from "@/components/ui/button";
 import {
   OrganizationSwitcher,
+  SignedIn,
   SignInButton,
   SignOutButton,
-  UserButton,
+  UserButton
 } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
 
 export function Header() {
   return (
-    <div className="border-b py-4 bg-gray-50">
+    <div className="relative z-10 border-b py-4 bg-gray-50">
       <div className="items-center container mx-auto justify-between flex">
         <Link href="/" className="flex">
-          <img
+          <Image
             width="50"
             height="50"
             alt="file drive logo"
@@ -21,9 +23,11 @@ export function Header() {
           <h1 className="text-2xl font-bolt flex my-auto">CloudVault</h1>
         </Link>
 
+        <SignedIn>
         <Button variant={"ghost"}>
           <Link href="/dashboard/files">Your files</Link>
         </Button>
+        </SignedIn>
 
         <div className="flex gap-2">
           <OrganizationSwitcher />
